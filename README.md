7ecc06c9d24ce53f70614390592d76063c375128
Toby lau <yatchol6214@hstat.org>
2019-03-05 19:53:20 +0000

    debug for .git

---
+++ README.md
@@ -0,0 +1,2 @@
+The idea of this project was to get the locate the user is in and find the nearest bus stop/ subway statation near them.
+
---
+++ models/model.rb
@@ -0,0 +1,22 @@
+# https://github.com/mjprude/livetrain_api
+# https://developers.google.com/transit/gtfs-realtime/gtfs-realtime-proto
+# http://datamine.mta.info/
+# https://github.com/tal/mta
+#https://developers.google.com/maps/solutions/store-locator/nyc-subway-locator
+
+
+# Google API 
+
+#https://console.cloud.google.com/freetrial/signup/billing/US?project=checkngo-1551456090086&redirectPath=https:%2F%2Fconsole.cloud.google.com%2Fgoogle%2Fmaps-apis%2Fonboard%3Fservices%3Dmaps-backend.googleapis.com%252Cstatic-maps-backend.googleapis.com%252Cstreet-view-image-backend.googleapis.com%252Cmaps-android-backend.googleapis.com%252Cmaps-ios-backend.googleapis.com%252Cstreetviewpublish.googleapis.com%252Cmaps-embed-backend.googleapis.com%26consoleUI%3DCLOUD%26project%3Dcheckngo-1551456090086&consoleReturnUrl=https:%2F%2Fcloud.google.com%2Fmaps-platform%2F%3Frefresh%3D1%26project%3Dcheckngo-1551456090086%26project%3Dcheckngo-1551456090086&consoleUI=MAPS
+#https://developers.google.com/maps/documentation/geocoding/start
+#https://developers.google.com/maps/documentation/javascript/importing_data
+#https://cloud.google.com/shell/docs/using-web-preview
+
+##shotgun -p $PORT -o $IP
+
+
+
+
+# subway station locator
+# https://developers.google.com/maps/solutions/store-locator/nyc-subway-locator
+#https://console.cloud.google.com/home/activity?cloudshell=true&project=checkngo-1551456090086
